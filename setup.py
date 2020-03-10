bc8706ec9521721661398a2dd877c88108bb9d28
cvb <ccvb3291@gmail.com>
2020-03-10 19:49:04 +0300

    Use correct deps

--- setup.py
+++ setup.py
@@ -9,18 +9,20 @@ setup(
         "console_scripts": [
         ],
     },
-    python_requires=">=3.7,<3.9",
+    python_requires=">=3.7,<3.8",
     install_requires=[
         "toolz ~= 0.9",
-        "fire ~= 0.2",
         "numpy ~= 1.18.1",
         "tensorflow ~= 2.1.0",
         "beautifulsoup4 ~= 4.8.2",
-        "regexp ~= 0.1",
+        "regex ~= 2020.2.20",
     ],
-    extra_require={
+    extras_require={
         "gpu": [
             "tensorflow-gpu ~= 2.1.0"
+        ],
+        "run": [
+            "fire ~= 0.2",
         ]
     }
 )
